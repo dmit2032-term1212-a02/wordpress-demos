870634d3a2b12dc676422eb878575ca924830190
lauren17 <lauren.olsen17@gmail.com>
2022-02-08 09:08:31 -0700

    archive - collection of posts, pages, categories, etc...

---
+++ archive.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * 
+ * This is the archive template
+ * This is the template that displays a collection of posts, categories, tags, or tag clouds, etc... It's a essentially a giant libraray of your site. 
+ * 
+ * @package 1212 wp demos A02
+ * @since 1.0
+ */
+    //display header
+    get_header()
+?>
+
+    <article <?php post_class();?> id="post-<?php the_ID();?>">
+
+    <h1 class="banner">
+        <?php _e('Archives'); ?>
+    </h1>
+    <div class="container">
+        <section>
+            <div class="row">
+                <div class="col col-1">
+                    <!-- recent posts -->
+                    <h2><?php _e('Recent Blogs');?></h2>
+                    <ul class="blogs">
+                        <!-- displays the 6 most recents posts 
+                        to display all posts: type=postbypost
+                         -->
+                        <?php wp_get_archives('type=postbypost&limit=6'); ?>
+                    </ul>
+
+                </div>
+                <div class="col col-2">
+                    <!-- recent posts -->
+                    <h2><?php _e('Pages');?></h2>
+                    <!-- displays a list of pages -->
+                    <ul class="page-list">
+                        <?php wp_list_pages(); ?>
+                    </ul>
+                </div>
+                <div class="col col-3">
+                    <!-- categories / tags -->
+                    <h2><?php _e('Categories');?></h2>
+                    
+                    <!-- display a list of categories -->
+                    <ul class="cat-list">
+                        <?php wp_list_categories('depth=1'); ?>
+                    </ul>
+                </div>
+                <div class="col col-4">
+                    <!-- monthly archives -->
+                    <h2><?php _e('Monthly Archives');?></h2>
+                    <!-- displays your monthly archives as list -->
+                    <?php the_widget('WP_Widget_Archives',
+                        array(
+                            'orderby'       => 'count',
+                            'order'         => 'DESC',
+                            'show_count'    => 1,
+                        )
+                    );?>
+
+                    <!-- display monthly archives as dropdown -->
+                    <?php the_widget('WP_Widget_Archives', 'dropdown=1'); ?>
+                </div>
+            </div>
+        </section>
+    </div>
+    </article>
+
+
+
+
+
+
+<?php get_footer(); ?>
