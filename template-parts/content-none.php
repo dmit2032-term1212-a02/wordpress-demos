9e31b0d7ce57df0c4aa60e1a15e0200d68b4ea8a
lauren17 <lauren.olsen17@gmail.com>
2022-02-10 11:09:45 -0700

    update does not exist template

---
+++ template-parts/content-none.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Template part/partial for displaying a content with no content.
+ * 
+ * @package 1212 wp demos A02
+ * @since 1.0.0
+ */
+?>
+
+<article <?php post_class();?> id="post-<?php the_ID();?>" >
+
+    <p><?php _e('Sorry this page does not exist'); ?></p>
+
+</article>
