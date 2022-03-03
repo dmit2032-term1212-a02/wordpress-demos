855ffafde4071bc0db0675f7fb493b7b2d39ef20
lauren17 <lauren.olsen17@gmail.com>
2022-03-03 11:09:23 -0700

    update header

--- header.php
+++ header.php
@@ -15,5 +15,35 @@
         <?php wp_head(); ?>
     </head>
     <body <?php body_class(); ?> >
-        <header></header>
+        <header>
+            <div class="container">
+                <div class="logo">
+                    <!-- option 2 -->
+                    <?php if ( ! has_custom_logo() ) { ?>
+                        <?php if ( is_front_page() && is_home() ) : ?>
+                            <!-- //if your page is set to front-page or blog display the site title (appearance > customize) -->
+                            <a rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" itemprop="url"><?php bloginfo( 'name' ); ?></a>
+                        <?php else : ?>
+                        <!-- //if your page is not set to front-page or blog display the site title (appearance > customize) -->
+                        <a rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" itemprop="url"><?php bloginfo( 'name' ); ?></a>
+                        <?php endif; ?>
+                            <!-- //otherwise display the custom logo. -->
+                        <?php } else {
+                            the_custom_logo();
+                        }?>
+                    </div>
+
+                    <!-- not the best method: device based mobile logo styling: wp_is_mobile() -->
+                    <div class="sm-screens mobile">
+                        <a class="mobile-logo" rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" itemprop="url">
+                            <img src="<?php bloginfo('template_directory'); ?>/assets/img/mobile-logo.svg" alt="<?php the_title(); ?>"/>
+                            <span class="sr-only"><?php bloginfo( 'name' ); ?></span>
+                        </a>
+                    </div>
+            </div>
+            <!-- //container -->
+
+
+
+        </header>
 
