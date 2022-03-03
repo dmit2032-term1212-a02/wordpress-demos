2cb57ee9befa18a9c27a636b2f8679e51c6bea2d
lauren17 <lauren.olsen17@gmail.com>
2022-03-03 11:10:52 -0700

    updated search

--- template-parts/content-search.php
+++ template-parts/content-search.php
@@ -9,12 +9,14 @@
 ?>
 
 <div class="">
+    <!-- displaying the title of the post or page -->
     <?php
         the_title(
             sprintf('<h2><a href="%s">', esc_url( get_permalink() ) ), '</a></h2>');
     ?>
-    
+    <!-- if statement below only displays additional content if it is a post type template. Pages will only display a title -->
     <?php if ( 'post' == get_post_type() ) : ?>
+        <?php echo get_the_post_thumbnail($post->ID, 'thumbnail'); ?>
         <p><?php the_date(); ?></p>
         <?php the_category(); ?>
         <p><?php the_excerpt(); ?></p>
