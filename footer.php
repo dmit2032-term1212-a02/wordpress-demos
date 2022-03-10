b6b9cdc69a3751a619b5304faf714d76076b13a7
lauren17 <lauren.olsen17@gmail.com>
2022-03-10 11:30:17 -0700

    update footer

--- footer.php
+++ footer.php
@@ -6,7 +6,22 @@
 */
 ?>
 
-        <footer></footer>
+        <footer>
+            <div class="container">
+                <div class="col col-one">
+                    <?php if (is_active_sidebar('footer-col-one') ) : ?>
+                        <?php dynamic_sidebar( 'footer-col-one' ); ?>
+                    <?php endif; ?>
+                </div>
+                <div class="col col-two">
+                    <?php if (is_active_sidebar('footer-col-two') ) : ?>
+                        <?php dynamic_sidebar( 'footer-col-two' ); ?>
+                    <?php endif; ?>
+                </div>
+                <!-- typical items that get hardcoded. -->
+                <p>Copyright &copy; <?php echo date('Y'); ?> WPdemos | For educational purposes  </p>
+             </div>
+        </footer>
     <?php wp_footer() ?>
     </body>
 </html>
