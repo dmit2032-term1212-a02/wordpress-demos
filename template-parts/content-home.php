9b3c562addfd73b0c8b019723fdb7511299f0f50
lauren17 <lauren.olsen17@gmail.com>
2022-03-29 09:48:11 -0600

    updated content-home.php with more acf fields

--- template-parts/content-home.php
+++ template-parts/content-home.php
@@ -16,6 +16,15 @@
     <!-- method #1 to display text field (heading) -->
     <h2><?php the_field('course_title'); ?></h2>
 
+    <!-- method #2 -->
+    <!-- displaying your heading as a variable --> 
+    <!-- set variable to name_field -->
+    <?php $course_title = get_field('course_title'); ?>
+    <h2>
+        <?php if($course_title) {
+            _e($course_title);
+        } ?>
+    </h2>    
 
 
     <!-- display custom content -->
@@ -38,6 +47,19 @@
         <?php wp_reset_postdata(); ?>
         <?php else : ?>
             <p><?php _e('Sorry, no post matched your criteria');?></p>
+    <?php endif; ?>
+
+    <!-- display a link/button to go the courses page -->
+    <!-- use acf relation link using array url method -->
+
+    <?php 
+        $courses_btn = get_field('course_view_all_button');
+        
+        if ( $courses_btn ) :
+            $course_btn_title = $courses_btn['title'];
+            $course_btn_url = $courses_btn['url']; ?>
+            <!-- create the link structure with the above variables -->
+            <a href="<?php print_r( esc_url( $course_btn_url) ); ?>"><?php print_r( esc_html( $course_btn_title ) ); ?></a>
     <?php endif; ?>
 
 
