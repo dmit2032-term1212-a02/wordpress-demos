4e242b988b0cd6c9ea92c480ff8b6569bb84270c
lauren17 <lauren.olsen17@gmail.com>
2022-04-08 00:44:00 -0600

    add post pagination

--- single.php
+++ single.php
@@ -6,29 +6,39 @@
  */
 
     //display header
-    get_header(); 
-?>
+    get_header(); ?>
+
+
     <h1 class="banner">
       <?php _e('Blog'); ?>
     </h1>
+
     <div class="full-container">
       <div class="container two-thirds-layout">
-        <!-- if there are posts then do something -->
-        <?php  if( have_posts() ) : ?>
-          <!-- while loop - is going to execute code as long as something is true. -->
-          <?php  while ( have_posts()) : the_post();
-            //add in template tags to display content
-            get_template_part('template-parts/content', 'single');
-          ?>
-          <?php endwhile; ?>
-          <!-- closes the while loop -->
+        <div class="two-thirds-col">
+          <!-- if there are posts then do something -->
+          <?php  if( have_posts() ) : ?>
+            <!-- while loop - is going to execute code as long as something is true. -->
+            <?php  while ( have_posts()) : the_post();
+              //add in template tags to display content
+              get_template_part('template-parts/content', 'single');
+            ?>
+            <?php endwhile; ?>
+            <!-- closes the while loop -->
+            <!-- posts pagination here - just after the loop ends -->
+            <!-- pagination -->
+            <ul class="pagination post">
+                <li><?php previous_post_link('%link', '&larr; Previous');?> </li>
+                <li><?php next_post_link('%link', 'Next &rarr;');?> </li>
+            </ul>
           <?php else: ?>
             <?php get_template_part('template-parts/content', 'none'); ?>
-        <?php endif; ?>
-        <!-- sidebar here -->
-          <div class="right-sidebar one-thrid-col">
-            <?php dynamic_sidebar('right-sidebar'); ?>
-          </div>
+          <?php endif; ?>
+          <!-- sidebar here -->
+        </div>
+        <div class="right-sidebar one-thrid-col">
+        <?php dynamic_sidebar('right-sidebar'); ?>
+        </div>
       </div>
     </div>
 
